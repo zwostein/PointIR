8636135148e7b57329bbddabbb5105e04100fbcf
zwostein <provisorisch@online.de>
2014-08-15 14:16:15 +0200

    updated help

--- src/pointird/main.cpp
+++ src/pointird/main.cpp
@@ -150,7 +150,7 @@ int main( int argc, char ** argv )
 
 		TCLAP::ValueArg<std::string> calibrationEndHookArg(
 			"", "endCalibHook",
-			"Script to execute when Calibration finished.\nDefaults to \"" + calibrationHook.getEndHook() + "\"",
+			"Script to execute when Calibration finished. An additional argument is appended on execution, indicating whether the calibration succeeded (1 for success, 0 for failure).\nDefaults to \"" + calibrationHook.getEndHook() + "\"",
 			false, calibrationHook.getEndHook(), "string", cmd );
 
 		TCLAP::ValueArg<std::string> deviceArg(
